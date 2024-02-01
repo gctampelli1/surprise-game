ebe76b96ca00cd730b21bcd1dfa41e015e5574f9
gctampelli1 <158405656+gctampelli1@users.noreply.github.com>
2024-02-01 01:37:42 -0300

    Add files via upload

---
+++ jogo.js
@@ -0,0 +1,8 @@
+function formValues() {
+    if(document.teste.field.value === 'teste'){
+        window.alert('Hmm, muito bem! Aqui está sua dica: ESPOSAS')
+        
+    } else {
+        window.alert('Tente novamente...acredito no seu potencial')
+    }
+}
